a0b9a44f19c8ba282d510ea17667f3295b80b619
Matt Wagner <matt.wagner@redhat.com>
2011-09-19 16:58:30 -0400

    Don't set :body in attrs
    Not only is this causing problems and not used by anything, but
    it's not clear to me that this is something we'd even want to do.

--- lib/aeolus_image/model/warehouse_client.rb
+++ lib/aeolus_image/model/warehouse_client.rb
@@ -58,7 +58,6 @@ module Aeolus
             next if att.match('-')
             attrs[att] = (@connection.do_request("#{@path}/#{att}", :plain => true) rescue nil)
           end
-          attrs[:body] = body
           attrs
         end
 
