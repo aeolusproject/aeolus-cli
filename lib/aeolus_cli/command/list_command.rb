cb6cf02ece8b57d149619e18f4ce4309a741af45
Martyn Taylor <mtaylor@redhat.com>
2011-10-18 13:40:31 +0100

    Updated List Command to use conductor API

--- lib/aeolus_cli/command/list_command.rb
+++ lib/aeolus_cli/command/list_command.rb
@@ -20,144 +20,141 @@ module Aeolus
       end
 
       def images
-        check_bucket_exists("images")
-        images = [["IMAGE ID", "LASTEST PUSHED BUILD", "NAME", "OS", "OS VERSION", "ARCH", "DESCRIPTION"]]
-        Image.all.each do |i|
-          images << [i.uuid, (i.latest_pushed_build.nil? ? "" : i.latest_pushed_build.uuid), i.name, i.os.name, i.os.version, i.os.arch, i.description]
+        begin
+          headers = ActiveSupport::OrderedHash.new
+          headers[:id] = "ID"
+          headers[:name] = "Name"
+          headers[:os] = "OS"
+          headers[:os_version] = "OS Version"
+          headers[:arch] = "Arch"
+          headers[:description] = "Description"
+          print_collection(Aeolus::CLI::Image.all, headers)
+          quit(0)
+        rescue => e
+          handle_exception(e)
         end
-        format_print(images)
-        quit(0)
       end
 
       def builds
-        builds = [["Build ID"]]
-        image = Image.find(@options[:id])
-        if image
-          image.image_builds.each do |b|
-            builds << [b.uuid]
-          end
-          format_print(builds)
-        else
-          puts "ERROR: could not find Image with ID: " + options[:id]
+        begin
+          headers = ActiveSupport::OrderedHash.new
+          headers[:id] = "ID"
+          headers[:image] = "Image"
+          collection = @options[:id].nil? ? Aeolus::CLI::Build.all : Aeolus::CLI::Build.find(:all, :from => Aeolus::CLI::Base.site.path + "/images/" + @options[:id] + "/builds.xml")
+          print_collection(collection, headers)
+          quit(0)
+        rescue => e
+          handle_exception(e)
         end
-        quit(0)
       end
 
       def targetimages
-        targetimages = [["Target Image Id"]]
-        build = ImageBuild.find(@options[:id])
-        if build
-          build.target_images.each do |ti|
-            targetimages << [ti.uuid]
-          end
-          format_print(targetimages)
-        else
-          puts "ERROR: could not find Build with ID: " + options[:id]
+        begin
+          headers = ActiveSupport::OrderedHash.new
+          headers[:id] = "ID"
+          headers[:status] = "Status"
+          headers[:build] = "Build"
+          collection = @options[:id].nil? ? Aeolus::CLI::TargetImage.all : Aeolus::CLI::TargetImage.find(:all, :from => Aeolus::CLI::Base.site.path + "/builds/" + @options[:id] + "/target_images.xml")
+          print_collection(collection, headers)
+          quit(0)
+        rescue => e
+          handle_exception(e)
         end
-        quit(0)
       end
 
       def providerimages
-        providerimages = [["PROVIDER IMAGE", "PROVIDER", "TARGET IMAGE", "TARGET IDENTIFIER"]]
-        target_image = TargetImage.find(@options[:id])
-        if target_image
-          target_image.provider_images.each do |pi|
-            providerimages << [pi.uuid, pi.provider_name, target_image.uuid, target_image.target]
-          end
-          format_print(providerimages)
-        else
-          puts "ERROR: could not find Target Image with ID: " + options[:id]
+        begin
+          headers = ActiveSupport::OrderedHash.new
+          headers[:id] = "ID"
+          headers[:target_identifier] = "Target Identifier"
+          headers[:provider] = "Provider"
+          headers[:status] = "Status"
+          headers[:target_image] = "Target Image"
+          collection = @options[:id].nil? ? Aeolus::CLI::ProviderImage.all : Aeolus::CLI::ProviderImage.find(:all, :from => Aeolus::CLI::Base.site.path + "/target_images/" + @options[:id] + "/provider_images.xml")
+          print_collection(collection, headers)
+          quit(0)
+        rescue => e
+          handle_exception(e)
         end
-        quit(0)
       end
 
       def targets
-        targets = [["NAME", "TARGET CODE"]]
-        targets << ["Mock", "mock"]
-        targets << ["Amazon EC2", "ec2"]
-        targets << ["RHEV-M", "rhevm"]
-        targets << ["VMware vSphere", "vsphere"]
-        targets << ["Condor Cloud", "condor_cloud"]
-        format_print(targets)
-        quit(0)
+        begin
+          headers = ActiveSupport::OrderedHash.new
+          headers[:name] = "Name"
+          headers[:deltacloud_driver] = "Reference"
+          print_collection(Aeolus::CLI::ProviderType.all, headers)
+          quit(0)
+        rescue => e
+          handle_exception(e)
+        end
       end
 
       def providers
-        print_values = [["NAME", "TYPE", "URL"]]
-
-        doc = Nokogiri::XML conductor['/providers'].get
-        doc.xpath("/providers/provider").each do |provider|
-          print_values << [provider.xpath("name").text, provider.xpath("provider_type").text, provider.xpath("url").text]
+        begin
+          headers = ActiveSupport::OrderedHash.new
+          headers[:name] = "Name"
+          headers[:provider_type] = "Type"
+          headers[:deltacloud_provider] = "Target Reference"
+          print_collection(Aeolus::CLI::Provider.all, headers)
+          quit(0)
+        rescue => e
+          handle_exception(e)
         end
-
-        format_print(print_values)
-        quit(0)
       end
 
       def accounts
-        print_values = [["NAME", "PROVIDER", "PROVIDER TYPE"]]
-        doc = Nokogiri::XML conductor['/provider_accounts/'].get
-        doc.xpath("/provider_accounts/provider_account").each do |account|
-          print_values << [account.xpath("name").text, account.xpath("provider").text, account.xpath("provider_type").text]
+        begin
+          headers = ActiveSupport::OrderedHash.new
+          headers[:name] = "Name"
+          headers[:provider] = "Provider"
+          headers[:provider_type] = "Provider Type"
+          print_collection(Aeolus::CLI::ProviderAccount.all, headers)
+          quit(0)
+        rescue => e
+          handle_exception(e)
         end
-
-        format_print(print_values)
-        quit(0)
       end
 
       private
-      # Takes a 2D array of strings and neatly prints them to STDOUT
-      def format_print(print_values)
-        widths =  Array.new(print_values[0].size, 0)
-        print_values.each do |print_value|
-          widths = widths.zip(print_value).map! {|width, value| value.length > width ? value.length : width }
-        end
+      # Print Collection to STDOUT in a column layout
+      def print_collection(collection, headers)
+        widths = column_widths(collection, headers)
 
-        print_values.each do |print_value|
-          widths.zip(print_value) do |width, value|
-            printf("%-#{width + 5}s", value)
+        # Print Headers
+        widths.each_pair do |header, width|
+          printf("%-#{width + 5}s", headers[header])
+        end
+        puts ""
+
+        # Print Underlines
+        widths.each_pair do |header, width|
+          printf("%-#{width + 5}s", "-" * width)
+        end
+        puts ""
+
+        # Print collection information
+        collection.each do |resource|
+          widths.each_pair do |attr, width|
+            printf("%-#{width + 5}s", resource.attributes.include?(attr) ? resource.attributes[attr].to_s : "")
           end
           puts ""
         end
+        puts ""
       end
 
-      def get_template_info(image, targetimage)
-        begin
-          template = Nokogiri::XML iwhd["/templates/" + iwhd["/target_images/" + targetimage + "/template"].get].get
-          [template.xpath("/template/name").text,
-            iwhd["/target_images/" + targetimage + "/target"].get,
-            template.xpath("/template/os/name").text,
-            template.xpath("/template/os/version").text,
-            template.xpath("/template/os/arch").text,
-            template.xpath("/template/description").text]
-        rescue
+      def column_widths(collection, headers)
+        widths = ActiveSupport::OrderedHash.new
+        headers.each_pair do |attr, header|
+          widths[attr] = header.length
         end
-      end
 
-      def get_image_name(image)
-        begin
-          template_xml = Nokogiri::XML iwhd["images/" + image].get
-          template_xml.xpath("/image/name").text
-        rescue
-          ""
+        collection.each do |resource|
+          headers.keys.each do |attr|
+            widths[attr] = widths[attr] > resource.attributes[attr].to_s.length ? widths[attr] : resource.attributes[attr].to_s.length
+          end
         end
-      end
-
-      def lastest_pushed(image)
-        begin
-          build = iwhd["/images/" + image + "/latest_build"].get
-          build.nil? ? "" : build
-        rescue
-          ""
-        end
-      end
-
-      def check_bucket_exists(bucket)
-        begin
-          iwhd["/" + bucket].get
-        rescue
-          nil
-        end
+        widths
       end
     end
   end
--- spec/command/list_command_spec.rb
+++ spec/command/list_command_spec.rb
@@ -17,6 +17,117 @@ require 'spec_helper'
 module Aeolus
   module CLI
     describe ListCommand do
+      it "should list all account" do
+        VCR.use_cassette('command/list_command/list_all_accounts') do
+          lc = ListCommand.new
+          begin
+            lc.accounts
+          rescue SystemExit => e
+            e.status.should == 0
+          end
+          headers = ["Name", "Provider", "Provider Type"]
+          content = ["ec2-acc", "ec2-us-east-1", "ec2"]
+          (headers + content).each do |expected_text|
+            $stdout.string.should include(expected_text)
+          end
+        end
+      end
+
+      it "should list all targets" do
+        VCR.use_cassette('command/list_command/list_all_targets') do
+          lc = ListCommand.new
+          begin
+            lc.targets
+          rescue SystemExit => e
+            e.status.should == 0
+          end
+          headers = ["Name", "Reference"]
+          content = ["Mock", "mock", "Amazon EC2", "ec2", "VMware vSphere", "vsphere"]
+          (headers + content).each do |expected_text|
+            $stdout.string.should include(expected_text)
+          end
+        end
+      end
+
+      it "should list all providers" do
+        VCR.use_cassette('command/list_command/list_all_providers') do
+          lc = ListCommand.new
+          begin
+            lc.providers
+          rescue SystemExit => e
+            e.status.should == 0
+          end
+          headers = ["Name", "Type", "Target Reference"]
+          content = ["ec2-us-east-1", "ec2", "us-east-1"]
+          (headers + content).each do |expected_text|
+            $stdout.string.should include(expected_text)
+          end
+        end
+      end
+
+      it "should list all images" do
+        VCR.use_cassette('command/list_command/list_all_images') do
+          lc = ListCommand.new
+          begin
+            lc.images
+          rescue SystemExit => e
+            e.status.should == 0
+          end
+          headers = ["ID", "Name", "OS", "OS Version", "Arch", "Description"]
+          content = ["7e6d409b-11d5-446d-99e8-656deb4f28ca", "a2da41eb-4bec-410c-b59d-afca5ea94c21", "Fedora", "14", "x86_64", "foo"]
+          (headers + content).each do |expected_text|
+            $stdout.string.should include(expected_text)
+          end
+        end
+      end
+
+      it "should list all build for a particular images" do
+        VCR.use_cassette('command/list_command/list_all_builds_for_image') do
+          lc = ListCommand.new({:id => "7e6d409b-11d5-446d-99e8-656deb4f28ca"})
+          begin
+            lc.builds
+          rescue SystemExit => e
+            e.status.should == 0
+          end
+          headers = ["ID", "Image"]
+          content = ["7e6d409b-11d5-446d-99e8-656deb4f28ca", "98632297-1d3b-4493-8643-e37beb29204c"]
+          (headers + content).each do |expected_text|
+            $stdout.string.should include(expected_text)
+          end
+        end
+      end
+
+      it "should list all target images for a particular build" do
+        VCR.use_cassette('command/list_command/list_all_target_images_for_build') do
+          lc = ListCommand.new({:id => "3eeb6c8f-9dd0-4a5b-bd13-e96daef435f9"})
+          begin
+            lc.targetimages
+          rescue SystemExit => e
+            e.status.should == 0
+          end
+          headers = ["ID", "Status", "Build"]
+          content = ["3a548835-3008-473b-a4c6-6618f82e026f", "COMPLETE", "3eeb6c8f-9dd0-4a5b-bd13-e96daef435f9"]
+          (headers + content).each do |expected_text|
+            $stdout.string.should include(expected_text)
+          end
+        end
+      end
+
+      it "should list all provider images for a particular target image" do
+        VCR.use_cassette('command/list_command/list_all_provider_images_for_target_image') do
+          lc = ListCommand.new({:id => "d7cab2b2-eeb8-459e-b739-a1844a5d5a62"})
+          begin
+            lc.providerimages
+          rescue SystemExit => e
+            e.status.should == 0
+          end
+          headers = ["ID", "Target Identifier", "Provider", "Status", "Target Image"]
+          content = ["9ac21235-81f4-4140-ac25-18a19ebcb2f8", "ami-test", "ec2-us-east-1", "COMPLETE", "d7cab2b2-eeb8-459e-b739-a1844a5d5a62"]
+          (headers + content).each do |expected_text|
+            $stdout.string.should include(expected_text)
+          end
+        end
+      end
     end
   end
 end
